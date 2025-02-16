797b96356710588c07a5599b5a666ababcd67165
BaneVredniMrav <branko1989stankovic@gmail.com>
2025-02-16 23:38:22 +0100

    fixes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -28,6 +28,17 @@ const navLinks = [
   { path: "/service-and-support", label: "Servis i Podrška" },
   { path: "/contact-us", label: "Kontakt" },
 ];
+
+const copyPhoneNumber = () => {
+  const phoneNumber = "+1234567890"; // Zamenite sa stvarnim brojem
+  navigator.clipboard.writeText(phoneNumber)
+    .then(() => {
+      alert("Broj telefona je sačuvan u memoriji!"); // Opcionalno, možete dodati obaveštenje
+    })
+    .catch((err) => {
+      console.error("Greška pri kopiranju broja:", err);
+    });
+};
 </script>
 
 <template>
@@ -54,11 +65,11 @@ const navLinks = [
         <!-- Desktop Menu -->
         <div class="flex flex-col items-center justify-center text-center">
           <div class="hidden lg:flex border-b max-w-xl ml-auto">
-            <div class="flex lg:space-x-6 xl:px-8">
-              <a href="tel:+1234567890" class="text-white hover:underline text-center flex items-center justify-center">
-                <i class="pi pi-phone font-semibold mr-2"></i> Pozovite nas
-              </a>
-              <a href="mailto:info@savapex.com" class="text-white hover:underline text-center flex items-center justify-center">
+            <div class="flex lg:space-x-6 xl:px-8 pb-2">
+              <button @click="copyPhoneNumber" class="text-white flex items-center justify-center">
+                <i class="pi pi-phone font-semibold mr-2"></i> 011/ 2-111-333
+              </button>
+              <a href="mailto:info@savapex.com" class="text-white text-center flex items-center justify-center">
                 <i class="pi pi-envelope font-semibold mr-2"></i> info@savapex.com
               </a>
             </div>
@@ -72,14 +83,14 @@ const navLinks = [
                 :class="[
                   isActiveLink(link.path) ? 'bg-slate-700' : 'hover:bg-slate-600 hover:text-white',
                   'text-white',
-  'xl:px-10',
+                  'xl:px-10',
                   'lg:px-6',
                   'py-2',
                   'mx-1',
                   'rounded-md',
-  'text-center',
-  'lg:text-base',
-  'xl:text-lg',
+                  'text-center',
+                  'lg:text-base',
+                  'xl:text-lg',
                   'flex',
                   'items-center',
                   'justify-center',
