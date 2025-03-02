7c9cda7b219c77ab117b571198e983dcf81dfb7d
BaneVredniMrav <branko1989stankovic@gmail.com>
2025-03-02 20:48:06 +0100

    Updates

---
+++ assets/AboutUsView-NnHFUNH8.js
@@ -0,0 +1,4 @@
+import{A as a}from"./AnimatedText-C_QRkBjF.js";import{O as o,a as e}from"./Our-mission-Bh7oYVB8.js";import{c as s,b as i,F as t,o as r}from"./index-DJVBsvVY.js";const c={__name:"AboutUsView",setup(n){return(m,u)=>(r(),s(t,null,[i(a,{title:"O firmi Savapex",subtitle:`Firma Savapex iz Kovačevca bavi se proizvodnjom, prodajom i ugradnjom industrijskih dizalica,
+        sa specijalizacijom u rešavanju složenih zahteva industrijskih postrojenja. Naša misija je da pružimo
+        visokokvalitetne proizvode koji omogućavaju efikasno i sigurno dizanje tereta u različitim industrijama.
+        Sa dugogodišnjim iskustvom i stručnim timom, Savapex je sinonim za pouzdanost i profesionalizam.`}),i(o),i(e)],64))}};export{c as default};
---
+++ assets/kran4-CKE3Vv_E.js
@@ -0,0 +1,1 @@
+const a="/savapex/assets/kran4-DiQ1umCA.jpg";export{a as k};
