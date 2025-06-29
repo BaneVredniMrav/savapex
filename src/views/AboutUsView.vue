de27aa493738608188dec4954c9b62dc2f6b1cc0
BaneVredniMrav <branko1989stankovic@gmail.com>
2025-06-29 18:52:02 +0200

    fix

--- src/views/AboutUsView.vue
+++ src/views/AboutUsView.vue
@@ -1,13 +1,8 @@
 <script setup>
 import AnimatedText from '@/components/AnimatedText.vue';
 import AboutUs from '@/components/About-us.vue';
-import OurVision from '@/components/Our-vision.vue';
-import OurMission from '@/components/Our-mission.vue';
 </script>
 
 <template>
 <AboutUs />
-  <!-- <OurVision />
-  <OurMission /> -->
-  <!-- https://alfakran.rs/o-nama/ -->
 </template>
