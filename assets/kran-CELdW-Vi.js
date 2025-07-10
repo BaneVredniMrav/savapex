50b1daadd5e8c9f0018fed459bfa6256c4f56d26
BaneVredniMrav <branko1989stankovic@gmail.com>
2025-07-10 22:54:31 +0200

    Updates

---
+++ assets/kran-CELdW-Vi.js
@@ -0,0 +1,1 @@
+const a="/savapex/assets/kran-D3UAABPQ.jpeg";export{a as k};
--- assets/kran5-Wck2bR07.js
+++
@@ -1,1 +0,0 @@
-const a="/savapex/assets/kran3-n6bVfSac.jpg",s="/savapex/assets/kran5-BsQZGFMZ.jpg";export{s as a,a as k};
